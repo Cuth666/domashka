fd69ba3eef9cc9fbd75810c85fad3edcd9723f9f
Cuth666 <56089021+Cuth666@users.noreply.github.com>
2019-12-25 22:39:09 +0300

    Update dz1.c

--- dz1.c
+++ dz1.c
@@ -1,219 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#define START 1
-#define MAXSIZE 50  //Максимальное число элементов 
-void work_with_set(char *set[]);  //Функция работы со множествами
-void main_menu();
-int main(int argc, char const *argv[])
-{
-	int c, n, i;
-	char *set1[MAXSIZE];	//Множество1
-	char *set2[MAXSIZE]; 	//Множество2
-	main_menu();
-	while((c = getchar()) != EOF)
-	{
-		i = 0;
-		while(c != '\n')
-		{
-			n = c;
-			c = getchar();
-			++i;
+#define STRINGSIZE 1024
+
+void printarray(char** array, int n) {
+	for (int i = 0; i < n; ++i)
+		printf("[%d]: %s\n", i+1, array[i]);
+}
+
+void array_add(char** array, int n) {
+	array[n] = malloc(STRINGSIZE*sizeof(char));
+	scanf("%s", array[n]);
+}
+
+void array_rem(char** array, int n) {
+	int id;	
+	printf("Which id we remove: \n");
+	scanf("%d", &id);
+	id--;
+	for (int i = id; i < n-1; i++)
+		array[i] = array[i+1];
+	array[n-1] = NULL;
+}
+
+int main() {
+	int n1, n2, MAXSIZE;
+	printf("Max size of arrays: ");
+	scanf("%d", &MAXSIZE);
+	printf("Enter num of elements of array 1: ");
+	scanf("%d", &n1);
+	getchar();
+	
+	char** array1 = malloc(MAXSIZE*sizeof(char*));
+	for (int i = 0; i < n1; ++i)
+		array1[i] = malloc(STRINGSIZE*sizeof(char));
+	
+	printf("Enter elements:\n");
+	for (int i = 0; i < n1; ++i)
+		scanf("%s", array1[i]);
+	
+	printf("Enter size of array 2: ");
+	scanf("%d", &n2);
+	getchar();
+	
+	char** array2 = malloc(MAXSIZE*sizeof(char*));
+	for (int i = 0; i < n2; ++i)
+		array2[i] = malloc(STRINGSIZE*sizeof(char));
+	
+	printf("Enter elements:\n");
+	for (int i = 0; i < n2; ++i)
+		scanf("%s", array2[i]);
+	
+	int cmd = -1;
+	while(cmd != 0) {
+		printf("Enter cmd:\n1. Add to 1\n2. Add to 2\n3. Remove from 1\n4. Remove from 2\n5. Print all\n");
+		scanf("%d", &cmd);
+		switch(cmd) {
+			case 0: break;
+			case 1: array_add(array1, n1); n1++;break;
+	case 2: array_add(array2, n2); n2++;break;
+			case 3: array_rem(array1, n1); n1--; break;
+			case 4: array_rem(array2, n2); n2--;break;
+			case 5: printf("Array 1:\n");printarray(array1, n1); printf("Array 2:\n");printarray(array2, n2); break;
+			default: printf("KEK\n"); break;
+			
 		}
-		if (c == '\n' && i == 1 && isdigit(n) != 0)
-		{
-			switch(n-'0'){
-				case 1:
-					work_with_set(set1);
-					break;
-				case 2:
-					work_with_set(set2);
-					break;
-				default:
-					printf("Ошибка! Введите 1 или 2\n\n");
-					break;
-			}
-		}
-		else
-		{
-			printf("Ошибка! Для выбора необходимо ввести 1 или 2\n\n");
-		}
-		main_menu();
+		
 	}
 	return 0;
-}
-void init_set(char *s_c[], int *pn); 		//Инициализация множества
-void print_elements(char *s_c[], int *pn); 	//Вывод элементов 
-void add_element(char *s_c[], int *pn); 	//Добавить элемент
-void del_element(char *s_c[], int *pn); 	//Удалить элемент
-void set_menu();
-void work_with_set(char *set[])
-{
-	int a = 0, n = 0;
-	int *pn = &n;
-	int stdbin;						/*При использовании scanf() символ перевода строки идёт в stdin.
-									Переменная 'stdbin' служит для забор '\n', т.е. очищения stdin*/
-	set_menu();	
-	while(a != 5 && n <= MAXSIZE)
-	{
-		scanf(" %d", &a);
-		stdbin = getchar();
-		switch(a){
-			case 1:
-				init_set(set, pn);
-				printf("\n");
-				set_menu();
-				break;
-			case 2:
-				print_elements(set, pn);
-				printf("\n");
-				set_menu();
-				break;
-			case 3:
-				add_element(set, pn);
-				printf("\n");
-				set_menu();
-				break;
-			case 4:
-				del_element(set, pn);
-				printf("\n");
-				set_menu();
-				break;
-			case 5:
-				break;
-			default:
-				printf("Введите число в соотвествии с меню\n");
-				break;
-		}
-	}
-	if (n > MAXSIZE)
-	{
-		printf("Слишком много элементов\n");
-	}
-	for (int i = 0; i < n; ++i)		//Цикл для очищения введеных элементов 
-	{
-		set[i] = 0;
-	}
-	return;
-}
-char *get_element();	//Получение элемента множества через ввод пользователя
-void init_set(char *s_c[], int *pn)
-{
-	if (*pn != 0)
-	{
-		printf("Множество уже проинициализировано\n");
-		return;
-	}
-	int c, stdbin;
-	printf("Проинициализируем множество. Сколько элементов нужно?\n");
-	scanf(" %d", &c);
-	while (c > MAXSIZE)
-	{
-		printf("Слишком много элементов. Максимальное кол-во: 50\n");
-		scanf(" %d", &c);
-	}
-	stdbin = getchar();
-	printf("Начинаем инициализацию. Элементы вводятся через Enter\n");
-	for (; *pn < c; ++(*pn))
-	{
-		s_c[*pn] = get_element();
-	}
-	return;
-}
-void print_elements(char *s_c[], int *pn)
-{
-	if (*pn == 0)
-	{
-		printf("Пустое множество\n");
-		return;
-	}
-
-	printf("Сейчас элементы множества это:\n");
-	for (int i = 0; i < *pn; ++i)
-	{
-		printf("%d) %s\n", i+1, s_c[i]);
-	}
-	return;
-}
-void add_element(char *s_c[], int *pn)
-{
-	if (*pn == 0)
-	{
-		printf("Пустое множество\n");
-		return;
-	}
-	printf("Добавить новый элемент\n");
-	s_c[(*pn)++] = get_element();
-	return;
-}
-void del_element(char *s_c[], int *pn)
-{
-	if (*pn == 0)
-	{
-		printf("Пустое множество\n");
-		return;
-	}
-	int i = 0, k = 0;
-	int del, stdbin;
-	char *unneccesary;			//Строка для удаления
-	char *set_copy[MAXSIZE];	//Копия множества без данного элемента
-	printf("Номер элемента, который вы хотите удалить: ");
-	scanf(" %d", &del);
-	stdbin = getchar();
-	unneccesary = s_c[del - 1];
-	for (; i < *pn; ++i)
-	{
-		if (s_c[i] != unneccesary)
-		{
-			(set_copy[k++] = s_c[i]);
-		}
-		s_c[i] = 0;
-	}
-	for (i = 0; i < k; ++i)		//Перезапись множества
-	{
-		s_c[i] = set_copy[i];
-	}
-	*pn = k;
-	return;
-}
-char *get_element()
-{
-	int c;
-	int len = 0;
-	char *s = (char*) malloc(START * sizeof(int));	//Создание динамического массива для строки произвольной длины
-	while((c = getchar()) != EOF && c != '\n')
-	{
-		if (isalnum(c) != 0)
-		{
-			s[len++] = c;
-			s = (char*) realloc(s, (len+1) * sizeof(int));
-		}
-	}
-	if (c == EOF)
-	{
-		free(s);
-		exit(0);
-	}
-	++len;
-	s[len] = '\0';
-
-	return s;
-}
-void main_menu()
-{
-	printf("Выберите множество с которым хотите работать:\n");
-	printf("1) Множество_1\n");
-	printf("2) Множество_2\n");
-	return;
-}
-void set_menu()
-{
-	printf("Выберите действие:\n");
-	printf("1) Инициализировать\n");
-	printf("2) Вывести значения\n");
-	printf("3) Добавить элемент\n");
-	printf("4) Удалить элемент\n");
-	printf("5) Выйти\n");
-	return;
 }
