bbb4c4bdd67bc9f2fdb492cca0baafa23372aa3b
Cuth666 <56089021+Cuth666@users.noreply.github.com>
2019-12-30 00:12:41 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -1,33 +1,73 @@
-#include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[])
-{
-    FILE *file;
-    file= fopen("graf.dot", "w");
-    if (file == NULL){
-        printf("Error");
-        exit(0);
+#include <stdio.h>
+
+int** a;
+int* color;
+
+void connect(int v, int n) {
+	color[v] = 1;
+	for (int j = 0; j < n; j++)
+		if (a[v][j] == 1 && color[j]!=1)
+			connect(j, n);
+}
+
+int main() {
+	// ZADANIE GRAFA
+    int n, t, s_min, k, r = 0;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+	color = (int*)malloc(n*sizeof(int));
+	for (int i = 0; i < n; i++) color[i] = 0;
+	
+    a = (int**)malloc(n*sizeof(int*));
+    for (int i = 0; i < n; i++)
+        a[i] = (int*)malloc(n*sizeof(int));
+	
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = -1;
+
+    printf("Enter the matrix\nX| ");
+    for (int j = 0; j < n; j++) printf("%d ", j+1);
+    printf("\n--");
+    for (int j = 0; j < n; j++) printf("--");
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d| ", i+1);
+        for (int j = 0; j < n; j++) {
+            if (j==i) a[i][j] = 0; //If we on the main diagonal, we put 0
+            if (a[i][j] == -1) { //If we dont enter this vertex
+                scanf("%d", &a[i][j]); //We enter it
+                a[j][i] = a[i][j]; //And set mirrored i j - j i same
+            } else
+                printf("%d ", a[i][j]); //Else we just print already entered value
+        }
     }
-    fprintf (file,"digraph test{\n");
-    //fprintf(file,"node [shape = box];\n");
-    int N,a,i,j;
-    printf("Vvedite col-vo vershin grafa: ");
-    scanf ("%d", &N);
-    int A[N][N];
-    for (i=0; i<N;i++){
-            for (j=0;j<N;j++){
-                scanf ("%d",&A[N][N]);
-                getchar();
-                if (A[N][N] > 0){
-                    fprintf(file,"%d->%d\n",i,j);
-                }
-                if (A[N][N]<0){
-                    fprintf(file,"%d->%d\n",j,i);
-                }
-            }
+    printf("\n");
+    //=========================================
+	//PROVERKA SVAZANNOSTI
+	connect(0, n);
+	int connect = 1;
+	for (int i = 0; i < n; i++)
+		if (color[i] == 0)
+			connect = 0;
+	if (connect == 1) printf("CONNECTIVE!\n");
+	else printf("NOT CONNECTIVE!\n");
+	//=========================================
+	//VISUAL
+	FILE* f = fopen("graph.txt", "w");
+	fprintf(f, "graph {\n    {1");
+	for (int i = 1; i < n; i++) fprintf(f, ", %d", i+1);
+	fprintf(f, "}\n");
+	for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (a[i][j] == 1) fprintf(f, "    %d -- %d;\n", i+1, j+1);
+        }
     }
-    fprintf(file,"}");
-    fclose(file);
-system("PAUSE");
-    return 0;
+	fprintf(f, "}");
+	fclose(f);
+	system("dot -Tpng graph.txt -o graph.png");
+	system("graph.png");
+	system("pause");
 }
